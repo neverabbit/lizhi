96fcc34e4bb9cac5d406ba0d7cf91b843383bbb7
neverabbit <neverrabbit@163.com>
2015-11-07 19:08:57 +0800

    banner11

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,7 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
-	
-	/*
-		Alpha by HTML5 UP
-		html5up.net | @n33co
-		Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-	*/
+
 
 	(function($) {
 

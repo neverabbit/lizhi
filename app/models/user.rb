74c9abe27002b776dff7d37b355eccf349891380
neverabbit <neverrabbit@163.com>
2015-12-21 23:30:57 +0800

    javascript validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
   has_many :recommenders, through: :passive_recommendations
   
   before_save { self.email = email.downcase unless email.nil? }
-  validates :phone, presence: { message: '手机号不能为空！' }, length: { is: 11, message: '请输入正确的手机号！' }, uniqueness: true
+  VALID_PHONE_REGEX = /(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1})|(14[0-9]{1}))+\d{8})/
+  validates :phone, presence: { message: '手机号不能为空！' }, length: { is: 11, message: '请输入正确的手机号！' }, uniqueness: true, format: { with: VALID_PHONE_REGEX, message: '请输入正确的手机号' }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, length: { maximum: 255 }, allow_blank: true, 
                           format: { with: VALID_EMAIL_REGEX }

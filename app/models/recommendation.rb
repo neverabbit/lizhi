d361dc278399e7959ae6d9c184a7eff82438c3c5
neverabbit <neverrabbit@163.com>
2015-12-23 15:26:35 +0800

    recom_form_comment

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -5,6 +5,7 @@ class Recommendation < ActiveRecord::Base
   
   validates :recommender_id, presence: true
   validates :recommendee_id, presence: true, uniqueness: { scope: :position, message: "该候选人已经被推荐过，欢迎推荐其他的人选！" }
+  validates :comment, presence: { message: "请填写推荐理由或候选人的关键词！" } 
   # validates :recommendee_id, 
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   
   before_save { self.email = email.downcase unless email.nil? }
   VALID_PHONE_REGEX = /(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1})|(14[0-9]{1}))+\d{8})/
-  validates :phone, presence: { message: '手机号不能为空！' }, length: { is: 11, message: '请输入正确的手机号！' }, uniqueness: true, format: { with: VALID_PHONE_REGEX, message: '请输入正确的手机号' }
+  validates :phone, presence: { message: '手机号不能为空！' }, length: { is: 11, message: '请输入正确的手机号！' }, uniqueness: true, format: { with: VALID_PHONE_REGEX, message: '请输入正确的手机号！' }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, length: { maximum: 255 }, allow_blank: true, 

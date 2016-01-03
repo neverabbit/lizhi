94d591f0177e265107a01d2accf7e3b5cbd9dbd3
neverabbit <neverrabbit@163.com>
2016-01-03 20:56:26 +0800

    resize_logo

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'faker'
 gem 'carrierwave'
-# gem 'mini_magick'
+gem 'mini_magick'
 gem 'fog'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -4,7 +4,11 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  # version :normal do
+  #   process efficient_conversion: [190, 190]
+  # end
+  process resize_and_pad: [190, 190]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -60,5 +64,19 @@ class LogoUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  
+  # private
+  #
+  #   def efficient_conversion(width, height)
+  #     manipulate! do |img|
+  #       img.combine_options do |c|
+  #         # c.fuzz        "3%"
+  #         # c.trim
+  #         c.resize      "#{width}x#{height}"
+  #         # c.resize      "#{width}x#{height}<"
+  #       end
+  #       img
+  #     end
+  #   end
 
 end
